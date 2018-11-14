5e30f073e469e1dd1e7988b15c0db3990fedf719
Ayoub <ayoub@MacBook-Pro-de-Ayoub.local>
2018-11-14 11:02:24 +0100

    fonctionnalité 2

--- utils_cv.py
+++ utils_cv.py
@@ -3,12 +3,22 @@ import numpy as np
 
 
 
+def load_and_display(file):
+   img=cv2.imread(file,0)
+   cv2.imshow('image', img)
+   cv2.waitKey(100000)
+   cv2.destroyAllWindows()
 
-img=cv2.imread(r'/Users/ayoub/yayaya-project/data/tetris_blocks.png', 0)
-cv2.imshow('image', img)
-cv2.waitKey(100000)
-cv2.destroyAllWindows()
 
 
+def process_image(filename): #rotation#
+    img = cv2.imread(filename,0)
+    rows,cols = img.shape
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+    dst = cv2.warpAffine(img,M,(cols,rows))
+    cv2.imshow('image',dst)
+    cv2.waitKey(10000)
+    cv2.destroyAllWindows()
+process_image(r'/Users/ayoub/yayaya-project/data/tetris_blocks.png')
 
 
