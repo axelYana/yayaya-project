f4cb9530ec6766e10dbea8fe291dca2a53027452
Ayoub <ayoub@MacBook-Pro-de-Ayoub.local>
2018-11-14 10:40:34 +0100

    premier exo

---
+++ utils_cv.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+
+
+
+img=cv2.imread(r'/Users/ayoub/yayaya-project/data/tetris_blocks.png', 0)
+cv2.imshow('image', img)
+cv2.waitKey(100000)
+cv2.destroyAllWindows()
+
+
+
+
